cbf5bd9c325fee2034b0b769fa597b841ef80f38
yingjieweb <1184061078@qq.com>
2020-03-22 16:07:29 +0800

    encapsulate jQuery

--- src/JQueryDom/jqdom.js
+++ src/JQueryDom/jqdom.js
@@ -1,19 +1,60 @@
-window.jQuery = function (selector) {
-  let nodes = document.querySelectorAll(selector); //伪数组
-
-  return {  //封装过程见optimize.js
-    addClass:function (className) { //addClass(){}
+window.$ = window.jQuery = function (selectorOrArray) {
+  let nodes;
+  if (typeof selectorOrArray === 'string'){
+    nodes = document.querySelectorAll(selectorOrArray);
+  }else if (selectorOrArray instanceof Array){
+    nodes = selectorOrArray;
+  }
+  return {
+    print:function(){
+      console.log(nodes);
+    },
+    find:function (selector) {
+      let array = [];
+      for (let i=0;i<nodes.length;i++){
+        array = array.concat(Array.from(nodes[i].querySelectorAll(selector)));
+      }
+      array.oldApi = this;
+      return jQuery(array);
+    },
+    each(fun){
+      for (let i=0;i<nodes.length;i++){
+        fun.call(null,nodes[i]);
+      }
+    },
+    parent(){
+      let parent = [];
+      this.each((node)=>{
+        if (parent.indexOf(node.parentNode) === -1)
+          parent.push(node.parentNode);
+      })
+      return jQuery(parent);
+    },
+    children(){
+      let children = [];
+      this.each((node)=>{
+        children.push(...node.children);
+      })
+      return jQuery(children);
+    },
+    siblings(){ //调用siblings()方法的api所操作的元素可能是一个数组
+      let siblings = [];  //所以需要用数组来存储其兄弟节点
+      let array = [];
+      this.each((node)=>{ //each遍历每一个节点
+        siblings.push(...Array.from(node.parentNode.children).filter(item => item!==node));
+        console.log(siblings);
+      })
+      return jQuery(siblings);
+    },
+    addClass:function (className) {
       for (let i=0;i<nodes.length;i++){
         nodes[i].classList.add(className);
       }
       return this;
     },
-    find:function (selector) {  //find(){}
-      let array = [];
-      for (let i=0;i<nodes.length;i++){
-        array = array.concat(Array.from(nodes[i].querySelectorAll(selector)));
-      }
-      return array;
+    oldApi:selectorOrArray.oldApi,
+    end:function(){
+      return this.oldApi;
     }
   }
 }
--- src/JQueryDom/main.js
+++ src/JQueryDom/main.js
@@ -1,10 +1,25 @@
 //return api
-let api = window.jQuery('.dom');  //window可省略，jQuery为全局变量
-console.log(api); //返回一个api对象，改对象可操作当前元素
+let api = window.jQuery('.dom');  //window可省略，jQuery为全局变量。返回一个api对象，该对象可操作当前元素
 
-//addClass
-jQuery('.dom').addClass('red');
+//addClass():添加class属性
+jQuery('.dom1').addClass('red'); //以dom1为类名的元素被添加了red类名
 
-//find
-let children = jQuery('.dom').find('.child1');
-console.log(children);
+//find():找当前元素的孩子
+jQuery('.dom2').find('.child1').addClass('blue');  //dom2>chicken的元素被添加了blue类名
+
+//end():返回上一级
+jQuery('.dom3').find('.child1').addClass('blue').end().addClass('orange');  //dom3被添加了orange类名
+
+//each():为每个元素添加一个回调函数
+jQuery('.dom4').find('.child1').each((node)=>{console.log(node)});  //<div class="child1">child1</div>
+
+//print() & parent()
+jQuery('.child1').parent().parent().print();
+
+//children
+jQuery('.dom5').children().print();
+
+console.log('1111111111111111111111111')
+//siblings
+jQuery('.dom6').siblings().print();
+
