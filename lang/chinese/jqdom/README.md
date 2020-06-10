32bba18c7617b08d3c1cf520956557aaf735cfdb
yingjieweb <1184061078@qq.com>
2020-06-10 12:36:19 +0800

    链式调用

--- lang/chinese/jqdom/README.md
+++ lang/chinese/jqdom/README.md
@@ -14,12 +14,30 @@
 &nbsp;&nbsp; 与 dombox 的封装思想略有不同，jQuery 是一个全局的特殊函数，用户可以通过 `window.jQuery('selector')` 的方式调用该函数，用于获取对应的元素，
 但是该函数的返回值却不是当前元素，而是返回一个对象，称为 jQuery 构造出来的对象，这个对象可以操作当前对应的元素，这就是 jQuery 的链式调用原理（闭包）。
 
-**1. 链式调用思想** - jQuery 在查找节点或操作节点后，并不会返回节点本身，而是返回一个可以操作当前节点的 api 对象，从而实现 jQuery 的链式风格。
+**1. 链式调用** - jQuery 在查找节点或操作节点后，并不会返回节点本身，而是返回一个可以操作当前节点的 api 对象，从而实现 jQuery 的链式风格。
 ```javascript
 window.jQuery = function (selector) {
   let nodes = document.querySelectorAll(selector);  //返回一个伪数组
   let api = {
-    addClass: function (className) { //这里用到闭包一直访问通过选择器获取的nodes
+    print: function () { 
+      console.log(nodes); //这里用到闭包一直访问通过选择器获取的 nodes
+      return api; //return api 返回能操作 nodes 的对象，为了链式调用
+    }
+  }
+  return api; //return api 返回能操作 nodes 的对象，为了链式调用
+}
+
+<div class="dom1"></div>
+window.jQuery('.dom1');  //jQuery 为全局变量，window 可省略。返回一个 api 对象，该对象可操作当前 .dom 元素
+window.jQuery('.dom1').print();  //NodeList [div.dom1]。 print 函数也返回一个 api 对象，这样即可一直链式调用下去
+```
+
+**2.jQuery('selector').addClass(className)** - 查找 selector 所匹配的元素，并给每个元素添加一个值为 className 的 class 属性。
+```javascript
+window.jQuery = function (selector) {
+  let nodes = document.querySelectorAll(selector);  //返回一个伪数组
+  let api = {
+    addClass: function (className) {
       for (let i = 0; i < nodes.length; i++) {
         nodes[i].classList.add(className);
       }
--- src/JQueryDom/main.js
+++ src/JQueryDom/main.js
@@ -1,25 +1,25 @@
 //return api
-let api = window.jQuery('.dom');  //window可省略，jQuery为全局变量。返回一个api对象，该对象可操作当前元素
+window.jQuery('.dom1');  //jQuery 为全局变量，window 可省略。返回一个 api 对象，该对象可操作当前 .dom 元素
+window.jQuery('.dom1').print();  //NodeList [div.dom1]。 print 函数也返回一个 api 对象，这样即可一直链式调用下去
 
 //addClass():添加class属性
-jQuery('.dom1').addClass('red'); //以dom1为类名的元素被添加了red类名
+//jQuery('.dom1').addClass('red'); //以dom1为类名的元素被添加了red类名
 
 //find():找当前元素的孩子
-jQuery('.dom2').find('.child1').addClass('blue');  //dom2>chicken的元素被添加了blue类名
+//jQuery('.dom2').find('.child1').addClass('blue');  //dom2>chicken的元素被添加了blue类名
 
 //end():返回上一级
-jQuery('.dom3').find('.child1').addClass('blue').end().addClass('orange');  //dom3被添加了orange类名
+//jQuery('.dom3').find('.child1').addClass('blue').end().addClass('orange');  //dom3被添加了orange类名
 
 //each():为每个元素添加一个回调函数
-jQuery('.dom4').find('.child1').each((node)=>{console.log(node)});  //<div class="child1">child1</div>
+//jQuery('.dom4').find('.child1').each((node)=>{console.log(node)});  //<div class="child1">child1</div>
 
 //print() & parent()
-jQuery('.child1').parent().parent().print();
+//jQuery('.child1').parent().parent().print();
 
 //children
-jQuery('.dom5').children().print();
+//jQuery('.dom5').children().print();
 
-console.log('1111111111111111111111111')
 //siblings
-jQuery('.dom6').siblings().print();
+//jQuery('.dom6').siblings().print();
 
