310577a51f6492724c42c2ef0d862e3ec7574d75
yingjieweb <1184061078@qq.com>
2020-06-10 16:11:10 +0800

    格式调整

--- src/JQueryDom/jqdom.js
+++ src/JQueryDom/jqdom.js
@@ -1,59 +1,59 @@
 window.$ = window.jQuery = function (selectorOrArray) {
   let nodes;
-  if (typeof selectorOrArray === 'string'){
+  if (typeof selectorOrArray === 'string') {
     nodes = document.querySelectorAll(selectorOrArray);
-  }else if (selectorOrArray instanceof Array){
+  } else if (selectorOrArray instanceof Array) {
     nodes = selectorOrArray;
   }
   return {
-    print:function(){
+    print: function () {
       console.log(nodes);
     },
-    find:function (selector) {
+    find: function (selector) {
       let array = [];
-      for (let i=0;i<nodes.length;i++){
+      for (let i = 0; i < nodes.length; i++) {
         array = array.concat(Array.from(nodes[i].querySelectorAll(selector)));
       }
       array.oldApi = this;
       return jQuery(array);
     },
-    each(fun){
-      for (let i=0;i<nodes.length;i++){
-        fun.call(null,nodes[i]);
+    each(fun) {
+      for (let i = 0; i < nodes.length; i++) {
+        fun.call(null, nodes[i]);
       }
     },
-    parent(){
+    parent() {
       let parent = [];
-      this.each((node)=>{
+      this.each((node) => {
         if (parent.indexOf(node.parentNode) === -1)
           parent.push(node.parentNode);
       })
       return jQuery(parent);
     },
-    children(){
+    children() {
       let children = [];
-      this.each((node)=>{
+      this.each((node) => {
         children.push(...node.children);
       })
       return jQuery(children);
     },
-    siblings(){ //调用siblings()方法的api所操作的元素可能是一个数组
+    siblings() { //调用siblings()方法的api所操作的元素可能是一个数组
       let siblings = [];  //所以需要用数组来存储其兄弟节点
       let array = [];
-      this.each((node)=>{ //each遍历每一个节点
-        siblings.push(...Array.from(node.parentNode.children).filter(item => item!==node));
+      this.each((node) => { //each遍历每一个节点
+        siblings.push(...Array.from(node.parentNode.children).filter(item => item !== node));
         console.log(siblings);
       })
       return jQuery(siblings);
     },
-    addClass:function (className) {
-      for (let i=0;i<nodes.length;i++){
+    addClass: function (className) {
+      for (let i = 0; i < nodes.length; i++) {
         nodes[i].classList.add(className);
       }
       return this;
     },
-    oldApi:selectorOrArray.oldApi,
-    end:function(){
+    oldApi: selectorOrArray.oldApi,
+    end: function () {
       return this.oldApi;
     }
   }
